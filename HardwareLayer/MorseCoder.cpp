97c65e6843067b41b6896f32defdf9be39cf10c7
darius <darius@vozbutas.lt>
2014-05-02 13:56:45 +0300

    getting MorseCoder class to compile, still segfoulting

--- HardwareLayer/MorseCoder.cpp
+++ HardwareLayer/MorseCoder.cpp
@@ -1,4 +1,19 @@
 
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+#include "sleep.h"
+
+
+void morseCallback(int state) {
+	// set pin state
+	printf("%d",state);
+}
+
+
 #ifndef _MORSECODER_H_
 #define _MORSECODE_H_
 
@@ -6,14 +21,17 @@ class MorseCoder {
 		static const int ON = 1;
 		static const int OFF = 0;
 		int dot_delay_ms;
-		void* callback;
-		char[256][8] morse_char_map;
+		//void* callback;
+		void(*callback)(int state);
+		char *morse_char_map[256];
 	public:
-		MorseCoder(int dot_delay_ms, void* callback);
+		MorseCoder(int dot_delay_ms, void(*callback)(int state));
         ~MorseCoder() {}
-        int send(char* message);
+        int send(const char* message);
     private:
-		void sendLetter(char letter);
+		void setupCharactersTable();
+		void sendWord(const char* word);
+		void sendLetter(const char letter);
 		void transmitDot();
 		void transmitDash();
 		void transmitSymbolSpace();
@@ -21,25 +39,27 @@ class MorseCoder {
 		void transmitWordSpace();
 		void transmitEndOfTransmission();
 		void transmitSymbol(int state, int ticks);
+};
 #endif
 
 
-int MorseCoder::send(char* message) {
+int MorseCoder::send(const char* message) {
+	printf("%s(%s)\n", __FUNCTION__, message);
 	// split string on spaces
 	// submit every word to
 	transmitEndOfTransmission(); 
 	return 0;
 }
-void MorseCoder::sendWord(char* word) {
+void MorseCoder::sendWord(const char* word) {
 	int i = 0;
 	while (word[i] != '\0') {
 		sendLetter(word[i]);
 	}
 	transmitWordSpace();
 }
-void MorseCoder::sendLetter(char letter) {
+void MorseCoder::sendLetter(const char letter) {
 	int i;
-	for	(i=0; i<8; i++) {
+	for	(i=0; i<9; i++) {
 		switch (morse_char_map[letter][i]) {
 			case '.':
 				transmitDot();
@@ -76,64 +96,69 @@ void MorseCoder::transmitWordSpace() {
 	transmitSymbol(OFF, 7-3); // one letter-space is included after each letter
 }
 void MorseCoder::transmitEndOfTransmission() {
+	printf("%s()\n", __FUNCTION__);
 	transmitSymbol(OFF, 13-7); // I invented this timming myself
 }
 void MorseCoder::transmitSymbol(int state, int ticks) {
-	callback(state);
+	printf("%s(%d, %d);\n", __FUNCTION__, state, ticks);
+	(*this->callback)(state);
+	printf("--- WE(%s) go past callback\n", __FUNCTION__);
 	msleep(dot_delay_ms * ticks);
 }
 
 
 
-MorseCoder::MorseCoder(int dot_delay_ms, void* callback) {
-	this.dot_delay_ms = dot_delay_ms;
-	this.callback = callback;
+MorseCoder::MorseCoder(int dot_delay_ms, void(*callback)(int)) {
+	dot_delay_ms = dot_delay_ms;
+	callback = callback;
 	setupCharactersTable();
 }
 void MorseCoder::setupCharactersTable() {
 	int i;
 	// set error code to all characters
-	morse_char_map[i] = {"........"}; 
+	for (i=0; i<256; i++) {
+		morse_char_map[i] = (char*)"........"; 
+	}
 
 	// define letters
-	morse_char_map['A'] = morse_char_map['a'] = ".-";
-	morse_char_map['B'] = morse_char_map['b'] = "-...";
-	morse_char_map['C'] = morse_char_map['c'] = "-.-.";
-	morse_char_map['D'] = morse_char_map['d'] = "-..";
-	morse_char_map['E'] = morse_char_map['e'] = ".";
-	morse_char_map['F'] = morse_char_map['f'] = "..-.";
-	morse_char_map['G'] = morse_char_map['g'] = "--.";
-	morse_char_map['H'] = morse_char_map['h'] = "....";
-	morse_char_map['I'] = morse_char_map['i'] = "..";
-	morse_char_map['J'] = morse_char_map['j'] = ".---";
-	morse_char_map['K'] = morse_char_map['k'] = "-.-";
-	morse_char_map['L'] = morse_char_map['l'] = ".-..";
-	morse_char_map['M'] = morse_char_map['m'] = "--";
-	morse_char_map['N'] = morse_char_map['n'] = "-.";
-	morse_char_map['O'] = morse_char_map['o'] = "---";
-	morse_char_map['P'] = morse_char_map['p'] = ".--.";
-	morse_char_map['Q'] = morse_char_map['q'] = "--.-";
-	morse_char_map['R'] = morse_char_map['r'] = ".-.";
-	morse_char_map['S'] = morse_char_map['s'] = "...";
-	morse_char_map['T'] = morse_char_map['t'] = "-";
-	morse_char_map['U'] = morse_char_map['u'] = "..-";
-	morse_char_map['V'] = morse_char_map['v'] = "...-";
-	morse_char_map['W'] = morse_char_map['w'] = ".--";
-	morse_char_map['X'] = morse_char_map['x'] = "-..-";
-	morse_char_map['Y'] = morse_char_map['y'] = "-.--";
-	morse_char_map['Z'] = morse_char_map['z'] = "--..";
+	morse_char_map['A'] = morse_char_map['a'] = (char*)".-";
+	morse_char_map['B'] = morse_char_map['b'] = (char*)"-...";
+	morse_char_map['C'] = morse_char_map['c'] = (char*)"-.-.";
+	morse_char_map['D'] = morse_char_map['d'] = (char*)"-..";
+	morse_char_map['E'] = morse_char_map['e'] = (char*)".";
+	morse_char_map['F'] = morse_char_map['f'] = (char*)"..-.";
+	morse_char_map['G'] = morse_char_map['g'] = (char*)"--.";
+	morse_char_map['H'] = morse_char_map['h'] = (char*)"....";
+	morse_char_map['I'] = morse_char_map['i'] = (char*)"..";
+	morse_char_map['J'] = morse_char_map['j'] = (char*)".---";
+	morse_char_map['K'] = morse_char_map['k'] = (char*)"-.-";
+	morse_char_map['L'] = morse_char_map['l'] = (char*)".-..";
+	morse_char_map['M'] = morse_char_map['m'] = (char*)"--";
+	morse_char_map['N'] = morse_char_map['n'] = (char*)"-.";
+	morse_char_map['O'] = morse_char_map['o'] = (char*)"---";
+	morse_char_map['P'] = morse_char_map['p'] = (char*)".--.";
+	morse_char_map['Q'] = morse_char_map['q'] = (char*)"--.-";
+	morse_char_map['R'] = morse_char_map['r'] = (char*)".-.";
+	morse_char_map['S'] = morse_char_map['s'] = (char*)"...";
+	morse_char_map['T'] = morse_char_map['t'] = (char*)"-";
+	morse_char_map['U'] = morse_char_map['u'] = (char*)"..-";
+	morse_char_map['V'] = morse_char_map['v'] = (char*)"...-";
+	morse_char_map['W'] = morse_char_map['w'] = (char*)".--";
+	morse_char_map['X'] = morse_char_map['x'] = (char*)"-..-";
+	morse_char_map['Y'] = morse_char_map['y'] = (char*)"-.--";
+	morse_char_map['Z'] = morse_char_map['z'] = (char*)"--..";
 
 	// define numbers
-	morse_char_map['0'] = "-----";
-	morse_char_map['1'] = ".----";
-	morse_char_map['2'] = "..---";
-	morse_char_map['3'] = "...--";
-	morse_char_map['4'] = "....-";
-	morse_char_map['5'] = ".....";
-	morse_char_map['6'] = "-....";
-	morse_char_map['7'] = "--...";
-	morse_char_map['8'] = "---..";
-	morse_char_map['9'] = "----.";
+	morse_char_map['0'] = (char*)"-----";
+	morse_char_map['1'] = (char*)".----";
+	morse_char_map['2'] = (char*)"..---";
+	morse_char_map['3'] = (char*)"...--";
+	morse_char_map['4'] = (char*)"....-";
+	morse_char_map['5'] = (char*)".....";
+	morse_char_map['6'] = (char*)"-....";
+	morse_char_map['7'] = (char*)"--...";
+	morse_char_map['8'] = (char*)"---..";
+	morse_char_map['9'] = (char*)"----.";
 
 }
 
@@ -144,11 +169,9 @@ int main() {
 	// setup pin
 	MorseCoder* mc = new MorseCoder(60, morseCallback);
 	printf("Sending something\n");
-	mc.send("MORSE");
+	mc->send("MORSE");
 	printf("done sending\n");
-}
-void morseCallback(int state) {
-	// set pin state
+	return 0;
 }
 
 
